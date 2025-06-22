155e825c317035d79f325d3f788d6106e73c76dd
santara9050 <mamadousantara48@gmail.com>
2025-06-22 23:25:57 +0000

    Update set.js

--- set.js
+++ set.js
@@ -2,14 +2,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
+  PREFIXE: process.env.PREFIXE || "🤴",
+  NOM_OWNER: process.env.NOM_OWNER || "𝑃𝐸𝑅𝐹𝐸𝐶𝑇_𝑂𝑁𝐸_𝐾𝐴𝑅𝐴",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "22351927475",
   MODE: process.env.MODE || "public",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_YR0xjyUe_SESSION-ID",
   LEVEL_UP: process.env.LEVEL_UP || "non",
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
+  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-Parfait",
+  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "𝑃𝐸𝑅𝐹𝐸𝐶𝑇_𝑂𝑁𝐸_𝐾𝐴𝑅𝐴",
   DATABASE: process.env.DATABASE,
   RENDER_API_KEY: process.env.RENDER_API_KEY,
   THEME: "1"
